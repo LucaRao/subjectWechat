f7a4a26ec24f035193ad0f0d3293c2cb0f48ec81
unknown <lucarao@163.com>
2022-08-22 17:45:44 +0800

    清空url和key

--- miniprogram/lib/supabase.ts
+++ miniprogram/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'supabase-wechat-stable'
-const url = "https://cbr2dbi5g6h4sltf7a8g.baseapi.memfiredb.com"
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImV4cCI6MzE5ODIxODk3NCwiaWF0IjoxNjYwMjk4OTc0LCJpc3MiOiJzdXBhYmFzZSJ9.N-8p-MyODWQjYd3-yGhngZDJusJpV7sBQZ-sEIHmzPw"
+const url = ""
+const key = ""
 
 export const supabase = createClient(url, key)
