8121e4f9424c33dfeac46279a88b5c387288de75
unknown <lucarao@163.com>
2022-08-22 17:45:19 +0800

    清空url和key

--- miniprogram/lib/supabase.ts
+++ miniprogram/lib/supabase.ts
@@ -1,6 +1,4 @@
-// import { createClient } from 'supabase-wechat-stable'
-import { createClient } from '../supabase-wechat-stable/src/index'
-// import { createClient } from '../memfireSupabase/supabase-js/src/index'
+import { createClient } from 'supabase-wechat-stable'
 const url = "https://cbr2dbi5g6h4sltf7a8g.baseapi.memfiredb.com"
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImV4cCI6MzE5ODIxODk3NCwiaWF0IjoxNjYwMjk4OTc0LCJpc3MiOiJzdXBhYmFzZSJ9.N-8p-MyODWQjYd3-yGhngZDJusJpV7sBQZ-sEIHmzPw"
 
