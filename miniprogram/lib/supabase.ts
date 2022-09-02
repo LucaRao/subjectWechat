ba6361da92da44c5f6c6db32a081a525035a5c47
LucaRao <56021673+LucaRao@users.noreply.github.com>
2022-09-02 17:52:05 +0800

    Update supabase.ts

--- miniprogram/lib/supabase.ts
+++ miniprogram/lib/supabase.ts
@@ -1,9 +1,4 @@
 import { createClient } from 'supabase-wechat-stable'
-// import { createClient } from '../supabase-wechat-stable/src/index'
-// import { createClient } from 'supabase-minichat'
-// import { createClient } from '../memfireSupabase/src/index'
-const url = "https://cbr2dbi5g6h4sltf7a8g.baseapi.memfiredb.com"
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImV4cCI6MzE5ODIxODk3NCwiaWF0IjoxNjYwMjk4OTc0LCJpc3MiOiJzdXBhYmFzZSJ9.N-8p-MyODWQjYd3-yGhngZDJusJpV7sBQZ-sEIHmzPw"
-// import { URL } from 'url-parse-wechat'
-// console.log(URL,'URL')
+const url = ""
+const key = ""
 export const supabase = createClient(url, key)
